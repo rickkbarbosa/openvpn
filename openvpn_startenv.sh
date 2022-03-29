7b4da8d5145358a94910cade9f4e6f44e2801307
Rickk Barbosa <rickkbarbosa@users.noreply.github.com>
2022-03-29 16:28:11 -0300

    adding dns

--- openvpn_startenv.sh
+++ openvpn_startenv.sh
@@ -5,9 +5,9 @@ OVPN_DATA=/opt/openvpn/data
 
 
 mkdir -p ${OVPN_DATA}
-docker run -v $OVPN_DATA:/etc/openvpn --rm ${DOCKER_OVPNIMAGE} ovpn_genconfig -u udp://${VPN_SERVERNAME} 
+docker run -v $OVPN_DATA:/etc/openvpn --rm ${DOCKER_OVPNIMAGE} ovpn_genconfig -u udp://${VPN_SERVERNAME} -p "dhcp-option DNS 8.8.8.8"
 docker run -v $OVPN_DATA:/etc/openvpn --rm -it ${DOCKER_OVPNIMAGE} ovpn_initpki nopass
 
 #Append routes
 #docker-compose run --rm openvpn rm /etc/openvpn/ovpn_env.sh
-#docker-compose run --rm openvpn ovpn_genconfig -N -d -n 192.168.13.6 -u udp://${VPN_SERVERNAME} -p "route 192.168.13.0 255.255.255.0" -p "route 172.17.0.0 255.255.0.0"
+#docker-compose run --rm openvpn ovpn_genconfig -N -d -n 192.168.13.6 -u udp://${VPN_SERVERNAME} -p "route 192.168.13.0 255.255.255.0" -p "route 172.17.0.0 255.255.0.0" -p "dhcp-option DNS 8.8.8.8"
